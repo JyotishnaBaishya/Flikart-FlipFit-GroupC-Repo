f3bddad31b5513b559bf2e5d98c3fe38f98c6157
jyotishna <jyotishna.baishya@BLRETV-9XPGQ3VX.local>
2023-09-17 15:13:00 +0530

    dump.sql

--- dump.sql
+++ dump.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.1.0, for macos13.3 (arm64)
 --
--- Host: localhost    Database: Flipfit
+-- Host: localhost    Database: FlipFit
 -- ------------------------------------------------------
 -- Server version	8.0.34
 
@@ -16,6 +16,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `Booking`
+--
+
+DROP TABLE IF EXISTS `Booking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Booking` (
+  `bookingID` int NOT NULL AUTO_INCREMENT,
+  `slotID` int NOT NULL,
+  `customerID` int NOT NULL,
+  PRIMARY KEY (`bookingID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Booking`
+--
+
+LOCK TABLES `Booking` WRITE;
+/*!40000 ALTER TABLE `Booking` DISABLE KEYS */;
+INSERT INTO `Booking` VALUES (1,2,1);
+/*!40000 ALTER TABLE `Booking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `Gym`
 --
 
@@ -25,13 +50,12 @@ DROP TABLE IF EXISTS `Gym`;
 CREATE TABLE `Gym` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `gymOwnerID` int DEFAULT NULL,
-  `gymName` varchar(45) NOT NULL,
   `location` varchar(45) DEFAULT NULL,
   `noOfSeats` varchar(45) DEFAULT NULL,
   `isApproved` tinyint DEFAULT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `ID_UNIQUE` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +64,7 @@ CREATE TABLE `Gym` (
 
 LOCK TABLES `Gym` WRITE;
 /*!40000 ALTER TABLE `Gym` DISABLE KEYS */;
-INSERT INTO `Gym` VALUES (1,1,'','BNG','10',1),(2,2,'Gym4','HYD','20',1),(3,55,'gym5','loc5','20',1),(4,55,'gym7','loc5','20',1),(5,55,'gym8','loc5','20',0),(6,55,'gym9','loc5','20',0),(7,55,'Gym1','loc5','20',0),(8,6,'Gym2','Bel','10',1),(9,6,'Gym3','Bel','11',2);
+INSERT INTO `Gym` VALUES (1,1,'loc1','20',0);
 /*!40000 ALTER TABLE `Gym` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -63,7 +87,7 @@ CREATE TABLE `GymOwner` (
   UNIQUE KEY `username_UNIQUE` (`username`),
   UNIQUE KEY `aadharcard_UNIQUE` (`aadharcard`),
   UNIQUE KEY `gstin_UNIQUE` (`gstin`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,36 +96,35 @@ CREATE TABLE `GymOwner` (
 
 LOCK TABLES `GymOwner` WRITE;
 /*!40000 ALTER TABLE `GymOwner` DISABLE KEYS */;
-INSERT INTO `GymOwner` VALUES (1,'GymOwner1','pass','1234-2345-1234','22AAAAA0000A1Z5',1),(2,'GymOwner2','pass2','1234-2345-1233','22AAAAA0000A1Z6',2),(6,'GymOwner3','pass3','4567-6543-1234','22AAAAA0000A1Z9',2),(14,'go','pass','4567-6543-9876','22AAAAA9987A1Z9',2);
+INSERT INTO `GymOwner` VALUES (1,'GymOwner1','pass','1234-2345-1234','22AAAAA0000A1Z5',0),(2,'GymOwner2','pass1','1239-2345-1234','22AAAAB0000A1Z5',0);
 /*!40000 ALTER TABLE `GymOwner` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Notification`
+-- Table structure for table `TimeSlot`
 --
 
-DROP TABLE IF EXISTS `Notification`;
+DROP TABLE IF EXISTS `TimeSlot`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Notification` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `userID` int DEFAULT NULL,
-  `userType` varchar(45) DEFAULT NULL,
-  `content` varchar(45) DEFAULT NULL,
-  `isViewed` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `TimeSlot` (
+  `slotID` int NOT NULL AUTO_INCREMENT,
+  `slotHour` int NOT NULL,
+  `gymID` int NOT NULL,
+  `availableSeats` int NOT NULL,
+  `day` datetime NOT NULL,
+  PRIMARY KEY (`slotID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Notification`
+-- Dumping data for table `TimeSlot`
 --
 
-LOCK TABLES `Notification` WRITE;
-/*!40000 ALTER TABLE `Notification` DISABLE KEYS */;
-INSERT INTO `Notification` VALUES (1,2,'Gymowner','Your Request has been Approved for Gym4','0'),(2,6,'Gymowner','Your Request has been Rejected for Gym3','0'),(3,2,'GymOwner','Something has happened','0');
-/*!40000 ALTER TABLE `Notification` ENABLE KEYS */;
+LOCK TABLES `TimeSlot` WRITE;
+/*!40000 ALTER TABLE `TimeSlot` DISABLE KEYS */;
+INSERT INTO `TimeSlot` VALUES (1,2,1,20,'2023-09-17 00:00:00'),(2,2,1,20,'2023-09-17 00:00:00');
+/*!40000 ALTER TABLE `TimeSlot` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -119,7 +142,7 @@ CREATE TABLE `User` (
   PRIMARY KEY (`ID`),
   UNIQUE KEY `ID_UNIQUE` (`ID`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,7 +151,7 @@ CREATE TABLE `User` (
 
 LOCK TABLES `User` WRITE;
 /*!40000 ALTER TABLE `User` DISABLE KEYS */;
-INSERT INTO `User` VALUES (1,'customer1','pass','Customer'),(2,'customer2','pass','Customer'),(3,'customer3','pass','Customer'),(4,'customer4','pass','Customer'),(6,'gymOwner99','pass','Gymowner'),(7,'admin','pass',NULL),(8,'gymOwer99','pass','Gymowner');
+INSERT INTO `User` VALUES (1,'customer1','pass','Customer'),(2,'customer2','pass','Customer'),(3,'customer3','pass','Customer'),(4,'customer4','pass','Customer');
 /*!40000 ALTER TABLE `User` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -141,4 +164,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-17 12:24:37
+-- Dump completed on 2023-09-17 15:11:30
